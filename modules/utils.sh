603cc7142a90239352697af8be9b9909c844e17e
Vonzen <77820687+Vonzhen@users.noreply.github.com>
2026-01-28 11:56:15 +0800

    Update utils.sh

--- modules/utils.sh
+++ modules/utils.sh
@@ -1,1 +1,30 @@
-modules
+#!/bin/sh
+
+# 颜色定义
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# 日志函数
+log_info() {
+    echo -e "${BLUE}[INFO]${NC} $1"
+}
+
+log_success() {
+    echo -e "${GREEN}[OK]${NC} $1"
+}
+
+log_warn() {
+    echo -e "${YELLOW}[WARN]${NC} $1"
+}
+
+log_error() {
+    echo -e "${RED}[ERR]${NC} $1"
+}
+
+# 简单的分割线
+print_line() {
+    echo "----------------------------------------------------------------"
+}
